fe63c783a80ac9ce0df01dbbfdca59b58b544dd1
Ellie Garcia <emg96@uw.edu>
2018-01-22 19:34:50 -0800

    moving along with HW2
    finished problems 4-6 of HW2

---
+++ combine-fasta.py
@@ -0,0 +1,11 @@
+import sys
+firstfastafile = open(sys.argv[1], "r")
+firstfastastring = firstfastafile.readlines()
+secondfastafile = open(sys.argv[2], "r")
+secondfastastring = secondfastafile.readlines()
+concatenatedfasta = firstfastastring + secondfastastring
+
+newfile = open(sys.argv[3], "w")
+newfile.write("%s"%concatenatedfasta)
+newfile.close()
+#need to test this
--- copy-file.py
+++ copy-file.py
@@ -2,6 +2,6 @@ import sys
 ogfile = open(sys.argv[1], "r")
 ogstring = ogfile.readlines()
 newfile = open(sys.argv[2], "w")
-newfile.write(%fogstring\n%)
+newfile.write("%s"%ogstring)
 newfile.close()
 print "File copying is complete"
