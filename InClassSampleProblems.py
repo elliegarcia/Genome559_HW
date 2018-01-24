e3e28e1acecc260d129a5a86ae207a6383e0bd69
elliegarcia <emg96@uw.edu>
2018-01-24 12:19:19 -0800

    In Class Sample Problems
    Worked on in class sample problems

--- InClassSampleProblems.py
+++ InClassSampleProblems.py
@@ -1,2 +1,13 @@
 import sys
-sys.argv[2].find(sys.argv[1])
+#total = 0
+#for num in sys.argv[1:]:
+#    integernum = int(num)
+#    total = total + integernum
+#    print total
+
+hellofile = open("Hello.txt", "r")
+
+for line in hellofile:
+    for word in line.split():
+        print len(word)
+hellofile.close()
