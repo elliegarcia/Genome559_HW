c53e121731ca89810c308a62bb5ce103757259bf
elliegarcia <emg96@uw.edu>
2018-02-06 10:41:02 -0800

    continued HW4

---
+++ blastn_parse.py
@@ -0,0 +1,4 @@
+import sys
+blastnoutfile = open(sys.argv[1], "r")
+blastnoutstring = blastnoutfile.read()
+print blastnoutstring
--- kmers.py
+++ kmers.py
@@ -1,4 +1,16 @@
 import sys
-chromosome21file = open(sys.argv[1], "r")
-chromosome21string = chromosome21file.read()
-print chromosome21string
+chromosome21file = open(sys.argv[1], "r") #opens the chromosome document
+chromosome21string = chromosome21file.read() #turn the chromosome document into a string
+chromosome21string = chromosome21string.upper() #make all the letters uppercase
+chromosome21string = chromosome21string.replace('\n', '') #get rid of \n in the string
+countsdictionary = {} #make a dictionary
+counts = 0 #start a counts
+for line in chromosome21string:
+    counts += 1
+    endmag = counts + int(sys.argv[2])
+    magnifier = chromosome21string[counts:endmag]
+    if magnifier in countsdictionary:
+        countsdictionary[magnifier] += 1
+    else:
+        countsdictionary[magnifier] = int(1)
+print countsdictionary
