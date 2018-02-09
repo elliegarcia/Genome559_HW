35d72830872b32b8b2098ed102b1c1d71ff79203
Ellie Garcia <emg96@uw.edu>
2018-02-09 13:38:47 -0800

    Fixed the master
    I pushed an old branch and lost some updates, so I updated the master branch. Let's see what happens with the other computer

--- calculate_aa_mass.py
+++ calculate_aa_mass.py
@@ -1,13 +1,33 @@
-import sys
+import sys  # IMPORTANT LIMITATION: THE QUERY MUST BE > 1
 massesfile = open(sys.argv[2],"r")
-aminoacidfile = open(sys.argv[3], "r")
-
 massesdictionary = {}
-for amino in massesfile:
+for amino in massesfile:  # put masses file in a dictionary
     line = amino.strip().split(",")
-    massscore = float(line[1])
+    massvalue = float(line[1])
     masskey = line[0]
-    massesdictionary[masskey] = massscore
+    massesdictionary[masskey] = massvalue
 
-answer = massesdictionary['A'] + massesdictionary['T']
-print answer
+nitrogensfile = open(sys.argv[3], "r")
+nitrogensdictionary = {}
+for nitrogen in nitrogensfile:  # put the nitrogens file in a dictionary
+    nline = nitrogen.strip().split(",")
+    nitrogenvalue = int(nline[1])
+    nitrogenkey = nline[0]
+    nitrogensdictionary[nitrogenkey] = nitrogenvalue
+
+query = list(sys.argv[1])
+count = 0
+monomass = massesdictionary[query[0]]  # made base number for the monoisotopic mass calculation
+for pep in range(1,len(query)):  # added the monoisotopic masses in the query together
+    count += 1
+    monomass = monomass + massesdictionary[query[count]]
+monomass = monomass + int(18)  # added the N-terminus weight to the mass
+
+count = 0
+nitrocount = nitrogensdictionary[query[0]] # made a base number for the nitrogen calculation
+for nitro in range(1,len(query)):  # added the nitrogens in the query together
+    count += 1
+    nitrocount += nitrogensdictionary[query[count]]
+nitrocount = nitrocount * float(0.997)
+heavymass = monomass + nitrocount  # calculated the heavymass
+print "The peptide ACDEK has a monoisotopic mass of %.3f" %monomass, "and a heavy-N mass of %.3f" %heavymass
--- kmers.py
+++ kmers.py
@@ -1,21 +1,26 @@
 import sys
-chromosome21file = open(sys.argv[1], "r") #opens the chromosome document
-chromosome21string = chromosome21file.read() #turn the chromosome document into a string
-print len(chromosome21string)
-chromosome21string = chromosome21string.upper() #make all the letters uppercase
-chromosome21string = chromosome21string.replace('\n', '') #get rid of \n in the string
-countsdictionary = {} #make a dictionary
-counts = 0 #start a counts
+chromosome21file = open(sys.argv[1], "r") # opens the chromosome document
+chromosome21string = chromosome21file.read() # turn the chromosome document into a string
+chromosome21string = chromosome21string.upper() # make all the letters uppercase
+chromosome21string = chromosome21string.replace('\n', '') # get rid of \n in the string
+chromosome21string = chromosome21string.replace('\r', '')
+
+endpoint = len(chromosome21string) - int(sys.argv[2]) # endpoint to keep the magnifier from running off
+countsdictionary = {} # make a dictionary
+counts = 0 # start a counter
 for line in chromosome21string:
-    endmag = counts + int(sys.argv[2]) #make the end of the magnifier
-    magnifier = chromosome21string[counts:endmag] #define the magnifier
-    counts += 1 # add a count +1 for ever loop
-    if magnifier in countsdictionary: #search for the magnifier in the dictionary
+    endmag = counts + int(sys.argv[2]) # make the end of the magnifier
+    magnifier = chromosome21string[counts:endmag] # define the magnifier
+    counts += 1 # increment the count by one to move the magnifier forward
+    if (counts > endpoint): # break the loop if the magnifier does not fit
+        break
+    if (magnifier in countsdictionary): # search for the magnifier in the dictionary
         countsdictionary[magnifier] += 1
     else:
         countsdictionary[magnifier] = int(1)
-countskeys = countsdictionary.keys()
-countskeys.sort()
+
+countskeys = countsdictionary.keys() # make a variable with just the keys
+countskeys.sort() # sort the keys
 for key in countskeys:
     print key + "\t" + str(countsdictionary[key])
 chromosome21file.close()
