46ac20d7cc3a36544d001776f5250bbaf24f4df1
elliegarcia <emg96@uw.edu>
2018-02-20 21:25:03 -0800

    HW 6
    finished problem 3!

--- matrix_math.py
+++ matrix_math.py
@@ -1,4 +1,3 @@
-import sys
 ## This function reads in a file containing a matrix of integers and returns it as a 2D list
 def make2dlist(mymatrixfile):
     matrixlist = [] # initialize the 2D list
@@ -10,7 +9,7 @@ def make2dlist(mymatrixfile):
         matrixlist.append(linelist) # add the list to the 2D list
     return matrixlist
 
-## This function takes a 2-D list of integers and prints it in a pretty format
+## This function takes a 2D list of integers and prints it in a pretty format
 def beautifymatrix(matrixlist):
     stringline = '' # initialize a string
     for line in matrixlist: # for each list in the bigger list
@@ -20,9 +19,9 @@ def beautifymatrix(matrixlist):
     stringline.strip() # strip the string
     return stringline
 
-## This function takes two matrix files, adds them together (entry-wise), and prints the result
+## This function takes 2 matrix files, adds them together (entry-wise), and prints the result
 def summatrix(matrixfile1, matrixfile2):
-    matrix1 = make2dlist(matrixfile1) # turns the matrix file into a list
+    matrix1 = make2dlist(matrixfile1) # turn the matrix file into a list
     matrix2 = make2dlist(matrixfile2)
     result = matrix1 # initialize the result
     for i in range(len(matrix1)): # iterate through rows
@@ -31,18 +30,24 @@ def summatrix(matrixfile1, matrixfile2):
     pretty_result = beautifymatrix(result) # make the matrix pretty
     return pretty_result
 
-########## Running the Functions ###########
-matrixfile = open(sys.argv[1], "r")
-matrixfile2p0 = open(sys.argv[1], 'r')
-answer = summatrix(matrixfile, matrixfile2p0)
-print answer
-# https://stackoverflow.com/questions/10508021/matrix-multiplication-in-python
-def matmult(a,b):
-    zip_b = zip(*b)
-    # uncomment next line if python 3 :
-    # zip_b = list(zip_b)
-    return [[sum(ele_a*ele_b for ele_a, ele_b in zip(row_a, col_b))
-             for col_b in zip_b] for row_a in a]
+## This Function takes 2 matrix files, multiplies them, and prints the result
+def multiplymatrix(matrixfileA, matrixfileB):
+    matrixA = make2dlist(matrixfileA) # turn the matrix files into lists
+    matrixB = make2dlist(matrixfileB)
+    matrixC = [[0 for row in range(len(matrixB[0]))] for col in range(len(matrixA))] # initialize the result
+    for i in range(len(matrixA)): # iterate through the rows of matrix A
+        for j in range(len(matrixB[0])): # iterate through the columns of matrix B
+            for k in range(len(matrixA[0])): # iterate through columns of matrix A
+                matrixC[i][j] += matrixA[i][k] * matrixB[k][j]
+    pretty_matrixC = beautifymatrix(matrixC)
+    return pretty_matrixC
 
-x = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
-y = [[1,2],[1,2],[3,4]]
+########## Running the Functions ###########
+import sys
+matrixfile = open(sys.argv[1], "r")
+matrixfile2 = open(sys.argv[2], 'r')
+print summatrix(matrixfile, matrixfile2)
+
+matrixfile = open(sys.argv[1], "r") # these are different files from the ones above
+matrixfile2 = open(sys.argv[2], 'r')
+print multiplymatrix(matrixfile, matrixfile2)
