d4a0b85ec48c651c435d1f67fda9ffa32f7b1e71
elliegarcia <emg96@uw.edu>
2018-03-06 20:49:17 -0800

    HW8 finished!
    I am done with homework for the quarter!

--- Date_Class.py
+++ Date_Class.py
@@ -25,42 +25,44 @@ class Date:
         self.year += (new_month-1)/12
         self.month = (new_month-1) % 12 + 1
 
-    def addDays(self, n=1): # This function adds n days to the current date by adding
-        # n to the current day and subtracting each subsequent month from the total
+    def addDays(self, n=1): # This function adds n days to the current date by replacing the current date
         new_day = self.day + n # add the current date + n
 
-        while new_day > 0:
-            if self.month > 12:
+        while new_day > 0: # this loop keeps iterating until new_day is less than the days in that month
+            if self.month > 12: # starts the month back at 1 after it reaches 12
                 self.year += self.month / 12
                 self.month = (self.month-1) % 12 + 1
             else:
                 self.month = self.month
 
-            if self.month==1 or self.month==3 or self.month==5 or self.month==7\
-                    or self.month==8 or self.month==10 or self.month == 12:
-                new_day = new_day - 31
-                if new_day <= 0:
-                    new_day += 31
-                    break
+            if self.month == 1 or self.month == 3 or self.month == 5 or self.month == 7\
+                    or self.month == 8 or self.month == 10 or self.month == 12: # if the month has 31 days
+                new_day = new_day - 31 # take off the days in that month from new_day
+                if new_day <= 0: # if new_day < the days in that month then
+                    new_day += 31 # add 31 back (to get a positive number)
+                    break # and break the loop
                 else:
-                    self.month += 1
-            elif self.month == 2:
-                new_day = new_day - 28
-                if new_day <= 0:
-                    new_day += 28
-                    break
+                    self.month += 1 # if new_day was > the days in that month than add +1 to the month
+            elif self.month == 2: # if the month has 28 days
+                new_day = new_day - 28 # take off the days in that month from new_day
+                if new_day <= 0: # if new_day < the days in that month then
+                    new_day += 28 # add 31 back (to get a positive number)
+                    break # and break the loop
                 else:
-                    self.month += 1
-            else:
-                new_day = new_day - 30
-                if new_day <= 0:
-                    new_day += 30
-                    break
+                    self.month += 1 # if new_day was > the days in that month than add +1 to the month
+            else: # if the month has 30 days
+                new_day = new_day - 30 # take off the days in that month from new_day
+                if new_day <= 0: # if new_day < the days in that month then
+                    new_day += 30 # add 30 back (to get a positive number)
+                    break # and break the loop
                 else:
-                    self.month += 1
-        self.day = new_day
+                    self.month += 1 # if new_day was > the days in that month than add +1 to the month
+        self.day = new_day # replace self.day with new_day
+
+#### Running the functions ####
 
 
-elmersbday = Date(9, 5, 1994)
-elmersbday.addDays(8694)
-print elmersbday
+mydate = Date(22, 11, 1976)
+mydate.printUK()
+mydate.addDays(5)
+mydate.printUK()
